6e0f7e3a7c5ed03a157183f02e089bb8a787ac9f
Jose Camino <jcamino@mandu.pe>
2019-02-26 15:13:07 -0500

    deploy docker service with containers kong cassandra comicbook

--- runall.sh
+++ runall.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+keyroute=$1
+
 terracom=$(whereis terraform|awk '{print $2}') 
 
-public_ip=$($terracom plan | grep "public_ip:"|awk '{print $2}'|tr -d "\"")
+$terracom apply -auto-approve
 
-echo "Estableciendo conexion con instancia  IP="$public_ip
+public_ip=$($terracom plan | grep "public_dns:"|awk '{print $2}'|tr -d "\"")
+
+echo "Estableciendo conexion con instancia  IP "$public_ip
+
+echo $public_ip >> hosts
+
+ssh-keygen -f $HOME/.ssh/known_hosts -R $public_ip
+
+sleep 10
+
+ansible-playbook docker_ans.yml -u ec2-user --private-key=$keyroute
 
