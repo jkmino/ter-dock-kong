27db93b590f356516854b7c09bd7d55ce4fa2e07
Jose Camino <jcamino@mandu.pe>
2019-02-27 12:14:34 -0500

    fix fully automation

--- runall.sh
+++ runall.sh
@@ -2,15 +2,15 @@
 keyroute=$1
 
 terracom=$(whereis terraform|awk '{print $2}') 
-
+$terracom init
 $terracom apply -auto-approve
 
 public_ip=$($terracom plan | grep "public_dns:"|awk '{print $2}'|tr -d "\"")
 
 echo "Estableciendo conexion con instancia  IP "$public_ip
 
-touch hosts
-echo $public_ip > hosts
+touch inventory.ini
+echo $public_ip > inventory.ini
 
 ssh-keygen -f $HOME/.ssh/known_hosts -R $public_ip
 
