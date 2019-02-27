fa024e939994d7ee33e2658dda7c890ea43ee7d9
Jose Camino <jcamino@mandu.pe>
2019-02-27 10:27:07 -0500

    fix kong deploy && fix runall script

--- runall.sh
+++ runall.sh
@@ -9,11 +9,14 @@ public_ip=$($terracom plan | grep "public_dns:"|awk '{print $2}'|tr -d "\"")
 
 echo "Estableciendo conexion con instancia  IP "$public_ip
 
-echo $public_ip >> hosts
+touch hosts
+echo $public_ip > hosts
 
 ssh-keygen -f $HOME/.ssh/known_hosts -R $public_ip
 
 sleep 10
 
 ansible-playbook docker_ans.yml -u ec2-user --private-key=$keyroute
+
+ssh -oStrictHostKeyChecking=no -i $keyroute ec2-user@$public_ip 'docker stats'
 
